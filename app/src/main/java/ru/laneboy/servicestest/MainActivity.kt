6a56ed29515c930e6bb03d818256b32fb7089f1f
LaneBoyy <kolomyjcevvladislav90@gmail.com>
2023-04-23 17:55:33 +0500

    Commit 3

--- app/src/main/java/ru/laneboy/servicestest/MainActivity.kt
+++ app/src/main/java/ru/laneboy/servicestest/MainActivity.kt
@@ -1,16 +1,11 @@
 package ru.laneboy.servicestest
 
-import android.app.Notification
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import android.os.Build
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import ru.laneboy.servicestest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
-    private var id = 0
 
     private val binding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
@@ -28,32 +23,10 @@ class MainActivity : AppCompatActivity() {
             startService(MyService.newIntent(this, 25))
         }
         binding.btnForegroundService.setOnClickListener {
-            showNotification()
-        }
-    }
-
-    private fun showNotification() {
-        val notificationChannel = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            NotificationChannel(
-                CHANNEL_NAME,
-                NotificationManager.IMPORTANCE_HIGH
+            ContextCompat.startForegroundService(
+                this,
+                MyForegroundService.newIntent(this)
             )
-        } else {
-            TODO("VERSION.SDK_INT < O")
         }
-        val notification = Notification.Builder(this)
-            .setContentTitle("Title")
-            .setContentText("Content text")
-            .setSmallIcon(R.drawable.ic_launcher_background)
-            .build()
-
-        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
-        notificationManager.notify(id++, notification)
-    }
-
-    companion object {
-
-        private const val CHANNEL_ID = "channel_id"
-        private const val CHANNEL_NAME = "channel_name"
     }
 }
